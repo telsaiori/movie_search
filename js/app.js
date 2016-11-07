f99c6cdebdc452cac517712a19669baa7909a074
telsaiori <telsaiori@gmail.com>
2016-11-07 12:15:07 +0000

    add link to IMDB

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ $(document).ready(function(){
        getJson();
     }); // keyup end
     
-    $('li').click()
-    
     function getJson(){
        var $title = $('#search').val();
        var $year = $('#year').val();
@@ -19,6 +17,7 @@ $(document).ready(function(){
        var data = {
          s: $title,
          y: $year,
+         plot: "full",
          r: "json"
        };
         $.getJSON(url,data,getMovie );
@@ -27,13 +26,14 @@ $(document).ready(function(){
     function getMovie(data){
         console.log(data);
         var html = "";
+        var modalHtml = "";
         if (data.Response === "True"){
         //'<li id=' + movie.imdbID + '>';
             $.each(data.Search,function(index, movie){
-                html += '<li class= "'+ movie.imdbID+'">';
+                html += '<li class= "'+ movie.imdbID+'" data-toggle="modal" data-target="#'+ movie.imdbID+'">';
                 html += '<div class="poster-wrap">';
                 if (movie.Poster === 'N/A'){
-                    html += "<i class='material-icons poster-placeholder'>crop_original</i>";
+                    html += '<a herf="#"><i class="material-icons poster-placeholder">crop_original</i></a>';
                 } else {
                     html += '<a herf="#"><img class="movie-poster" src="' + movie.Poster + '"></a>';
                 }
@@ -42,6 +42,13 @@ $(document).ready(function(){
                 html += '<span class="movie-title">'+ movie.Title +'</span>';
                 html += '<span class="movie-year">' + movie.Year + '</span>';
                 html += "</li>";
+                modalHtml += '<div class="modal fade" id="'+ movie.imdbID+'" role="dialog"><div class="modal-dialog"> <div class="modal-content"><div class="modal-header">';
+                modalHtml += '<h4 class="modal-title">' + movie.Title+'</h4><p>' + movie.Year+'</p></div>';
+                modalHtml += '<div class="modal-body"><img src="'+ movie.Poster+'">';
+                modalHtml += '<p>'+ movie.Plot +'</p></div>';
+                modalHtml += '<button class="btn btn-default"><a class="link-imdb" href="http://www.imdb.com/title/'+ movie.imdbID +'" target="_blank">Link to IMDB</a></button>';
+                modalHtml += '<div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>';
+                
             });
         } else {
 
@@ -51,6 +58,7 @@ $(document).ready(function(){
         }
             
        $('#movies').html(html);
+       $('.main-content').append(modalHtml);
         
     } //getMovie end
            
