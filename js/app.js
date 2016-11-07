1893ddf6589ba58f5958b25360c27cd127e9c138
telsaiori <telsaiori@gmail.com>
2016-11-07 17:05:04 +0000

    add ajax for update modal page info

--- js/app.js
+++ js/app.js
@@ -10,6 +10,24 @@ $(document).ready(function(){
        getJson();
     }); // keyup end
     
+    $('.main-title').click(function(e){
+        alert('hi');
+        var $title = $(this).attr('class');
+        var $year = $('#year').val();
+        var url = "https://www.omdbapi.com/?";
+        var data = {
+          t: $title,
+          y: $year,
+          plot: "full",
+          r: "json"
+        };
+        $.ajax({
+            url: url,
+            data: data,
+            success: getMovie
+        });
+    }); //click end
+    
     function getJson(){
        var $title = $('#search').val();
        var $year = $('#year').val();
@@ -22,6 +40,7 @@ $(document).ready(function(){
        };
         $.getJSON(url,data,getMovie );
     }
+    
             
     function getMovie(data){
         console.log(data);
@@ -48,6 +67,8 @@ $(document).ready(function(){
                 modalHtml += '<p>'+ movie.Plot +'</p></div>';
                 modalHtml += '<button class="btn btn-default"><a class="link-imdb" href="http://www.imdb.com/title/'+ movie.imdbID +'" target="_blank">Link to IMDB</a></button>';
                 modalHtml += '<div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>';
+
+                    
                 
             });
         } else {
@@ -59,6 +80,33 @@ $(document).ready(function(){
             
        $('#movies').html(html);
        $('.main-content').append(modalHtml);
+       
+       $('li').click(function(){
+           var updateModal = "";
+            $.ajax({
+                url: "https://www.omdbapi.com/?",
+                data: {
+                    i: $(this).attr('class'),
+                    plot: "full",
+                    r: "json"
+                },
+                success: function(data){
+                    if (data.Response === "True"){
+                         updateModal += '<div class="modal-dialog"> <div class="modal-content"><div class="modal-header">';
+                         updateModal += '<h4 class="modal-title">' + data.Title+'</h4><p>' + data.Year+'</p></div>';
+                         updateModal += '<div class="modal-body"><img src="'+ data.Poster+'">';
+                         updateModal += '<p>'+ data.Plot +'</p></div>';
+                         updateModal += '<button class="btn btn-default"><a class="link-imdb" href="http://www.imdb.com/title/'+ data.imdbID +'" target="_blank">Link to IMDB</a></button>';
+                         updateModal += '<div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="data">Close</button></div></div></div></div>';
+
+                    }
+                    
+                    $('#'+ data.imdbID ).html(updateModal);
+                }
+                
+                
+            });
+       }); //click end
         
     } //getMovie end
            
